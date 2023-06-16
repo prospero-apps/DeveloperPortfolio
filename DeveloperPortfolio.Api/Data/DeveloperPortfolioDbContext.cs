a85c189ba546c4e1a97b8e30a19a3bf535c02b28
prospero-apps <48125733+prospero-apps@users.noreply.github.com>
2023-06-16 14:14:16 +0200

    Create the general structure of the solution and add the basic Entity classes.

---
+++ DeveloperPortfolio.Api/Data/DeveloperPortfolioDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DeveloperPortfolio.Api.Data
+{
+    public class DeveloperPortfolioDbContext : DbContext
+    {
+        public DeveloperPortfolioDbContext(DbContextOptions<DeveloperPortfolioDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- DeveloperPortfolio.Api/Entities/Link.cs
+++ DeveloperPortfolio.Api/Entities/Link.cs
@@ -6,5 +6,6 @@
         public string Destination { get; set; }
         public string DisplayText { get; set; }
         public string Icon { get; set; }
+        public int ProjectId { get; set; }
     }
 }
