0ac964f1b040dbefadce27df6686f1b5fe3aad64
prospero-apps <48125733+prospero-apps@users.noreply.github.com>
2023-06-17 13:07:00 +0200

    Create the Models project and add dto classes to it.

---
+++ DeveloperPortfolio.Models/Dtos/LinkDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeveloperPortfolio.Models.Dtos
+{
+    public class LinkDto
+    {
+        public int Id { get; set; }
+        public string Destination { get; set; }
+        public string DisplayText { get; set; }
+        public string Icon { get; set; }
+        public int ProjectId { get; set; }
+    }
+}
