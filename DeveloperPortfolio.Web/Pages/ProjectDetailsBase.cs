d9607d3df3a9ace6a9e6160792438ff813b1079e
prospero-apps <48125733+prospero-apps@users.noreply.github.com>
2023-06-18 22:54:00 +0200

    Create the ProjectDetails razor component and the corresponding base class.

---
+++ DeveloperPortfolio.Web/Pages/ProjectDetailsBase.cs
@@ -0,0 +1,30 @@
+﻿using DeveloperPortfolio.Models.Dtos;
+using DeveloperPortfolio.Web.Services.Contracts;
+using Microsoft.AspNetCore.Components;
+
+namespace DeveloperPortfolio.Web.Pages
+{
+    public class ProjectDetailsBase : ComponentBase
+    {
+        [Parameter]
+        public int Id { get; set; }
+
+        [Inject]
+        public IProjectService ProjectService { get; set; }
+
+        public ProjectDto Project { get; set; }
+        public string ErrorMessage { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                Project = await ProjectService.GetProject(Id);
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+            }
+        }
+    }
+}
