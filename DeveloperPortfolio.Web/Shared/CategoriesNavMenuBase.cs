274de73e8fd73fed2d4ed5cb25ddad3a82aa6399
prospero-apps <48125733+prospero-apps@users.noreply.github.com>
2023-06-29 17:29:18 +0200

    Create nav links to the admin pages and to products by category.

---
+++ DeveloperPortfolio.Web/Shared/CategoriesNavMenuBase.cs
@@ -0,0 +1,28 @@
+﻿using DeveloperPortfolio.Models.Dtos;
+using DeveloperPortfolio.Web.Services.Contracts;
+using Microsoft.AspNetCore.Components;
+
+namespace DeveloperPortfolio.Web.Shared
+{
+    public class CategoriesNavMenuBase : ComponentBase
+    {
+        [Inject]
+        public ICategoryService CategoryService { get; set; }
+
+        public IEnumerable<CategoryDto> CategoryDtos { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                CategoryDtos = await CategoryService.GetAllCategories();
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+            }
+        }
+    }
+}
